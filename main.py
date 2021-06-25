8b8b6e23015dd1a94e7ee80c75cd6c5d3f5c0368
Harshal0908 <harshal.humane1999@gmail.com>
2021-06-25 14:42:17 +0530

    ONE HOT REPRESENTAION OF THE WORDS

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 import pandas as pd
-
+import tensorflow as tf
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Bidirectional
+from tensorflow.keras.layers import Dropout
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+#importing the dataset
 df = pd.read_csv(r'E:\Natural Language Processing Projects\Fake news Classifier\train.csv')
+
+#droping NA values
 df = df.dropna()
+
 X = df.drop('label',axis=1)
 Y = df['label']
+
+voc_size = 5000
+
+messages = X.copy()
+messages.reset_index(inplace=True)
+ps = PorterStemmer()
+corpus = []
+
+for i in range(0,len(messages)):
+    #removing special charachters
+    review = re.sub('[^a-zA-Z]',' ',messages['title'][i])
+    #converting all letters in lowercase
+    review = review.lower()
+    #spliting all the words
+    review = review.split()
+    #removing stopwords
+    review = [(word) for word in review if not word in stopwords.words('english')]
+    #joining the words with " ", creating the sentences and appending it in corpus
+    review = ' '.join(review)
+
+    corpus.append(review)
+#all the words in corpus is represented in one hot representation
+onehot_repr=[one_hot(words,voc_size) for words in corpus]
 
 
 
